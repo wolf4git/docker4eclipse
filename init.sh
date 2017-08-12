94910b9948378d5b81aadc299066e1ac494db4cd
Wolfgang Vonnemann <wolfgang.vonnemann@gmail.com>
2017-08-12 14:55:51 +0200

    Added change on dir docker4eclipse

--- init.sh
+++ init.sh
@@ -8,16 +8,16 @@
 #      an icon on ~/bin/icons
 #
 #... the startup-script is expecting your eclipse-oxygen docker
-#    to be started from ~/dockers/eclipse-oxygen/run.sh
+#    to be started from ~/dockers/docker4eclipse/run.sh
 #
 if [ ! -e ~/bin ]; then mkdir ~/bin; fi
 if [ ! -e ~/bin/icons ]; then mkdir ~/bin/icons; fi
 #
 if [ ! -e ~/dockers ]; then mkdir ~/dockers; fi
-if [ ! -e ~/dockers/eclipse-oxygen ]; then mkdir ~/dockers/eclipse-oxygen; fi
+if [ ! -e ~/dockers/docker4eclipse ]; then mkdir ~/dockers/docker4eclipse; fi
 #
 # copy startup-script and icon to be used on Desktop
-cp -r userHome/bin/ ~/bin
+cp -r userHome/bin ~
 #
 # finally copy userHome/Desktop/Oxygen.desktop to your Desktop-Folder
 # ~/Desktop  or on german installation ~/Schreibtisch/
