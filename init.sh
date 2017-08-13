8422ba87226647fbebf01de25f6d4aad45a4b510
Wolfgang Vonnemann <wolfgang.vonnemann@gmail.com>
2017-08-13 14:54:46 +0200

    adjusted init.sh

--- init.sh
+++ init.sh
@@ -7,23 +7,28 @@
 #... the startup-script is expecting your eclipse-oxygen docker
 #    to be started from ~/dockers/docker4eclipse/run.sh
 #
-if [ ! -e ~/bin ]; then mkdir ~/bin; fi
-if [ ! -e ~/bin/icons ]; then mkdir ~/bin/icons; fi
+if [ ! -e ~/bin ]; then mkdir ~/bin 2>/dev/null; fi
+if [ ! -e ~/bin/icons ]; then mkdir ~/bin/icons 2>/dev/null; fi
 #
-if [ ! -e ~/dockers ]; then mkdir ~/dockers; fi
-if [ ! -e ~/dockers/docker4eclipse ]; then mkdir ~/dockers/docker4eclipse; fi
+if [ ! -e ~/dockers ]; then echo "!!! the directory ~/dockers should exist. "; fi
+if [ ! -e ~/dockers/docker4eclipse ]; then echo "!!! the directory ~/dockers/docker4eclipse should exist, here should be your git-clone"; fi
 #
 #
 # copy startup-script and icon to be used on Desktop
-cp -r userHome/bin ~
+cp -r userHome/bin/* ~/bin
 
 #
 #... the desktopfile can be copied to your Desktop-folder
 #... this desktopfile is expecting:
 # create Desktopfile
-. ./userHome/Desktop/build.sh
+cd userHome/Desktop
+. build.sh
 
 # finally copy userHome/Desktop/Oxygen.desktop to your Desktop-Folder
 # ~/Desktop  or on german installation ~/Schreibtisch/
-cp ./userHome/Desktop/Oxygen.desktop ~/Desktop
+echo "working on $PWD"
+cp Oxygen.desktop ~/Desktop/Oxygen.desktop 2>/dev/null
+if [ ${?} -ne 0 ]; then
+   cp Oxygen.desktop ~/Schreibtisch
+fi
 #
--- userHome/Desktop/build.sh
+++ userHome/Desktop/build.sh
@@ -2,6 +2,7 @@
 # -------------------------------------------------------
 
 # -------------------------------------------------------
+echo "...building Oxygen.desktop"
 cat Oxygen.desktop.org | sed "s#<USER_HOME>#${HOME}#g" > Oxygen.desktop
 chmod 770 Oxygen.desktop
 # -------------------------------------------------------
